e3f1aab6b98e7a65733d88608be613a2aa7ef24d
yagnikbeladiya <165830630+yagnikbeladiya@users.noreply.github.com>
2024-06-18 16:16:51 +0530

    Add files via upload

---
+++ inheritanseExample/src/inheritanseExample/baseclass.java
@@ -0,0 +1,20 @@
+package inheritanseExample;
+
+public class baseclass {
+	
+	private int amt;
+	
+	public int getamt() {
+		return amt;
+	}
+	
+	public void setamt(int amt) {
+		this.amt=amt;
+		
+	}
+	
+	public String toString() {
+		return "baseclass[amt=" + amt +"]";
+	}
+
+}
